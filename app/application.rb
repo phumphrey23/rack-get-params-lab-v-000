47e086e3bd8a50236a393407629b0cd89be1899f
Precious Humphrey <phumphrey23@gmail.com>
2018-11-10 17:54:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        "Empty Cart"
+        resp.write "Empty Cart"
       else
         @@cart.each do |item|
           resp.write "#{item}\n"
